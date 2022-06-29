d374df6e48dc8d9a3f753761ee4a4f7f24956861
Yong Jie <yyj_1986@hotmail.com>
2022-06-29 08:20:39 +0000

    Workshop 2

---
+++ workshop02/Readme.md
@@ -0,0 +1,5 @@
+Step 1: 
+`terraform apply`
+
+Step 2:
+`ansible-playbook -i inventory.yaml playbook.yaml`
---
+++ workshop02/variables.tf
@@ -0,0 +1,32 @@
+variable "DO_token" {
+  type      = string
+  sensitive = true
+}
+
+variable "DO_upload_public_ssh_key" {
+  type = string
+}
+
+variable "ssh_private_key_path" {
+  type = string
+}
+
+variable "DO_name" {
+  type    = string
+  default = "vs-code-server"
+}
+
+variable "DO_image" {
+  type    = string
+  default = "ubuntu-20-04-x64"
+}
+
+variable "DO_region" {
+  type    = string
+  default = "SGP1"
+}
+
+variable "DO_size" {
+  type    = string
+  default = "s-1vcpu-2gb"
+}
